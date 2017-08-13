Fixed the predict function and added an important note about [0,1] scaling the domain X.

gam.py
@@ -17,8 +17,6 @@ class AdditiveModel():
         self.lambdas = None
         self.S = None
         self.B = None
-        self.mins = None
-        self.maxes = None
         self.X_aug = None
         self.Y_aug = None
         self.lr = None
@@ -33,19 +31,6 @@ class AdditiveModel():
         
     def set_X(self,X):
         self.X = X
-        self.mins = {}
-        self.maxes = {}
-        number_of_rows,number_of_columns = self.X.shape
-        for j in xrange(number_of_columns):
-            self.mins[j] = []
-            self.maxes[j] = []
-            for i in xrange(number_of_rows):
-                x = self.X[i][j]
-                self.mins[j].append(x)
-                self.maxes[j].append(x)
-                
-            self.mins[j] = min(self.mins[j])
-            self.maxes[j] = max(self.maxes[j])
         
     def set_Y(self,Y):
         self.Y = Y
@@ -61,26 +46,13 @@ class AdditiveModel():
         self.Y_name = Y_name
         
     def run(self):
-        print 'BUILDING S...'
         self._build_S()
-        
-        print 'BUILDING B...'
         self._build_B()
-        
-        print 'BUILDING X...'
         self.build_X()
-        
-        print 'BUILDING Y...'
         self._build_Y()
-        
-        print 'ESTIMATING PARAMETERS...'
         self.lr = LinearRegression()
         self.lr.fit(self.X_aug,self.Y_aug)
-        
-        print 'GENERATE COEFFICIENTS...'
         self._generate_coefficients()
-        
-        print 'GENERATE PREDICTIONS...'
         self._generate_predictions()
         
     def plot(self):
@@ -115,59 +87,57 @@ class AdditiveModel():
         plot.scatter(X,self.Y - all_predicted_Y[0:number_of_rows],color='blue',s=1.0)
         plot.show()
         
-    #def predict(self,X,Y,include_plot=False):
-    #    X_aug = self.build_X(X)
-    #    raw_predictions = []
-    #    predicted_Ys = []
-    #    number_of_rows = X_aug.shape[0] - self.B.shape[0]
-    #    number_of_columns = X.shape[1]
-    #    for i in xrange(number_of_columns):
-    #        coefficients = self.coefficients[i]
-    #        self.lr.coef_ = coefficients
-    #        raw_prediction = self.lr.predict(X_aug)
-    #        raw_predictions.append(raw_prediction)
+    def predict(self,X,Y,include_plot=False):
+        X_aug = self.build_X(X)
+        raw_predictions = []
+        predicted_Ys = []
+        number_of_rows = X_aug.shape[0] - self.B.shape[0]
+        number_of_columns = X.shape[1]
+        for i in xrange(number_of_columns):
+            coefficients = self.coefficients[i]
+            self.lr.coef_ = coefficients
+            raw_prediction = self.lr.predict(X_aug)
+            raw_predictions.append(raw_prediction)
             
-    #    self.overall_mean = float(sum(self.Y[:,0])/len(self.Y[:,0]))
+        self.overall_mean = float(sum(self.Y[:,0])/len(self.Y[:,0]))
             
-    #    l = len(raw_predictions)
-    #    for i in xrange(l):
-    #        mean = self.means[i]
-    #        predicted_Y = raw_predictions[i] - mean + self.overall_mean
-    #        predicted_Ys.append(predicted_Y)
+        for i in xrange(number_of_columns):
+            mean = self.means[i]
+            predicted_Y = raw_predictions[i] - mean + self.overall_mean
+            predicted_Ys.append(predicted_Y)
             
+        if include_plot:
+            number_of_rows,number_of_columns = X.shape
+            for i in xrange(number_of_columns):
+                X_col = X[:,i]
+                X_name = self.X_names[i]
+                predicted_Y = predicted_Ys[i]
             
-    #    if include_plot:
-    #        number_of_rows,number_of_columns = X.shape
-    #        for i in xrange(number_of_columns):
-    #            X_col = X[:,i]
-    #            X_name = self.X_names[i]
-    #            predicted_Y = self.predicted_Y[i]
-    #        
-    #            plot.xlabel(X_name)
-    #            plot.ylabel(self.Y_name)
-    #            plot.title('Variable '+str(i+1))
-    #            plot.scatter(X_col,Y,color='blue',s=0.5)
-    #            plot.scatter(X_col,predicted_Y[0:number_of_rows,0],color='orange',s=10.0)
-    #            plot.show()
+                plot.xlabel(X_name)
+                plot.ylabel(self.Y_name)
+                plot.title('Prediction')
+                plot.scatter(X_col,Y,color='blue',s=0.5)
+                plot.scatter(X_col,predicted_Y[0:number_of_rows,0],color='orange',s=10.0)
+                plot.show()
             
-    #    all_predicted_Y = copy(predicted_Ys[0])
-    #    for i in xrange(1,len(predicted_Ys)):
-    #        predicted_Y = predicted_Ys[i]
-    #        all_predicted_Y = all_predicted_Y + predicted_Y - self.overall_mean
-    #        
-    #    errors = []
-    #    absolute_error = 0.0
-    #    number_of_rows = Y.shape[0] # NOTE: DO NOT USE all_predicted_Y!
-    #    for i in xrange(number_of_rows):
-    #        y_est = all_predicted_Y[i][0]
-    #        y_act = Y[i][0]
-    #        diff = y_act - y_est
-    #        errors.append(diff)
-    #        absolute_error = absolute_error + abs(diff)
+        all_predicted_Y = copy(predicted_Ys[0])
+        for i in xrange(1,len(predicted_Ys)):
+            predicted_Y = predicted_Ys[i]
+            all_predicted_Y = all_predicted_Y + predicted_Y - self.overall_mean
+            
+        errors = []
+        absolute_error = 0.0
+        number_of_rows = Y.shape[0]
+        for i in xrange(number_of_rows):
+            y_est = all_predicted_Y[i][0]
+            y_act = Y[i][0]
+            diff = y_act - y_est
+            errors.append(diff)
+            absolute_error = absolute_error + abs(diff)
         
-    #    prediction = (predicted_Ys,all_predicted_Y,errors,absolute_error)
+        prediction = (predicted_Ys,all_predicted_Y,errors,absolute_error)
         
-    #    return prediction
+        return prediction
         
     def _generate_predictions(self):
         raw_predictions = []
@@ -288,17 +258,6 @@ class AdditiveModel():
         X_aug = np.array([1.0]*number_of_rows_X*number_of_columns_X)
         X_aug.resize((number_of_rows_X,number_of_columns_X))
         
-        # scale the explanatory variables so they have the same domain as the basis functions.
-        for j in xrange(number_of_columns):
-            x_min = min(X[:,j])
-            x_max = max(X[:,j])
-            #x_min = self.mins[j]
-            #x_max = self.maxes[j]
-            
-            for i in xrange(number_of_rows):
-                x = X[i][j]
-                X[i][j] = (x - x_min)/x_max
-        
         for i in xrange(number_of_rows):
             j_star = 0
             for j in xrange(number_of_columns):
@@ -396,4 +355,40 @@ class AdditiveModel():
         self.set_variable_names(['X1','X2','X3'],'Response')
         self.run()
         self.plot()
+        
+    def generate_example_3(self,sample_size=1000):
+        X_train = []
+        Y_train = []
+        X_test = []
+        Y_test = []
+        for i in xrange(sample_size):
+            x = random.uniform(0,1)
+            y = 3.0*abs(np.cos(np.pi*(x-0.75))) + random.gauss(0,1.0)
+            rand = random.uniform(0,1)
+            if rand < 0.20:
+                X_test.append(x)
+                Y_test.append(y)
+            else:
+                X_train.append(x)
+                Y_train.append(y)
+        
+        X_train = np.array(X_train)
+        X_train.resize((len(X_train),1))
+        Y_train = np.array(Y_train)
+        Y_train.resize((len(Y_train),1))
+        
+        X_test = np.array(X_test)
+        X_test.resize((len(X_test),1))
+        Y_test = np.array(Y_test)
+        Y_test.resize((len(Y_test),1))
+        
+        self.set_X(X_train)
+        self.set_Y(Y_train)
+        self.set_knots([[0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9]])
+        self.set_lambdas([0.01])
+        self.set_variable_names(['X1'],'Response')
+        self.run()
+        self.plot()
+        
+        val = self.predict(X_test,Y_test,True)
         